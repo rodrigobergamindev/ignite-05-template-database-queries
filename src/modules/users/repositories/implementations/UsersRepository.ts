935ed14912d93caf1350580287491fc1fba91fd0
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2022-12-22 09:35:20 -0300

    first commit

---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,41 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async findUserWithGamesById({
+    user_id,
+  }: IFindUserWithGamesDTO): Promise<User> {
+    const user = await this.repository
+    .findOne({id:user_id}, {relations:["games"]});
+
+    if(!user) throw new Error("User does not exists!")
+
+    return user
+  }
+
+  async findAllUsersOrderedByFirstName(): Promise<User[]> {
+    const users = await this.repository.query('SELECT*FROM users ORDER BY first_name');
+    return users;
+  }
+
+  async findUserByFullName({
+    first_name,
+    last_name,
+  }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+    return this.repository.query(`
+      SELECT * FROM users
+      WHERE LOWER(first_name)=LOWER('${first_name}')
+      AND LOWER(last_name)=LOWER('${last_name}')
+      LIMIT 1
+    `);
+  }
+}
